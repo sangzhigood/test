b28d3152022e82fac7b45925d6f46db45b8ff2cb
sangzhigood <sangzhigood@gmail.com>
2013-12-17 00:30:54 +0800

    ddd

--- src/main/java/com/sangzhi/App.java
+++ src/main/java/com/sangzhi/App.java
@@ -1,10 +1,9 @@
 package com.sangzhi;
 
+import java.lang.reflect.InvocationTargetException;
+import java.lang.reflect.Method;
 import java.net.MalformedURLException;
 import java.net.URL;
-import java.net.URLClassLoader;
-
-import com.thoughtworks.xstream.XStream;
 
 /**
  * Hello world!
@@ -12,25 +11,31 @@ import com.thoughtworks.xstream.XStream;
 public class App {
 
     public static void main(String[] args) throws MalformedURLException, InstantiationException,
-                                          IllegalAccessException, ClassNotFoundException {
+                                          IllegalAccessException, ClassNotFoundException, SecurityException,
+                                          NoSuchMethodException, IllegalArgumentException, InvocationTargetException {
+        // 生成文本
         URL url112 = new URL("file:///home/sangzhi/test/resources/xstream-1.1.2.jar");
         URL[] url112s = new URL[] { url112 };
-        URLClassLoader url1 = new URLClassLoader(url112s);
-        XStream xStream = (XStream) url1.loadClass("com.thoughtworks.xstream.XStream").newInstance();
+        XClassLoader url1 = new XClassLoader(url112s);
+        Class class112 = url1.loadClass("com.thoughtworks.xstream.XStream");
+        Method m = class112.getMethod("toXML", Object.class);
+
+        TransferDO transferDO = new TransferDO();
         StringBuilder sb = new StringBuilder();
         sb.append("sang");
         sb.append("dddd");
-        System.out.println(xStream.toXML(sb));
+        transferDO.setSb(sb);
+        Object object = m.invoke(class112.newInstance(), transferDO);
+        System.out.println(object);
+        Method m121s = class112.getMethod("fromXML", String.class);
+        Object object121 = m121s.invoke(class112.newInstance(), object.toString());
         URL url141 = new URL("file:///home/sangzhi/test/resources/xstream-1.4.1.jar");
         URL[] url141s = new URL[] { url141 };
-        URLClassLoader url2 = new URLClassLoader(url141s);
-        XStream xStream141 = (XStream) url2.loadClass("com.thoughtworks.xstream.XStream").newInstance();
-        StringBuilder sb1 = new StringBuilder();
-        sb1.append("sang");
-        sb1.append("dddd");
-        System.out.println(xStream141.toXML(sb1));
-        StringBuilder sb141 = (StringBuilder) xStream141.fromXML(xStream.toXML(sb));
-        System.out.println(sb141.toString());
+        XClassLoader url2 = new XClassLoader(url141s);
+        Class class141 = url2.loadClass("com.thoughtworks.xstream.XStream");
+        Method m1412 = class141.getMethod("toXML", Object.class);
+        String sb141 = (String) m1412.invoke(class141.newInstance(), object121);
+        System.out.println(sb141);
 
     }
 }
